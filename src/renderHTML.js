282e03e8d2e2419d3ff4c09f36d6e3a4b34f18a9
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-08-18 12:36:07 -0700

    renderHTML.js done -- untested

--- src/renderHTML.js
+++ src/renderHTML.js
@@ -1,5 +1,5 @@
 // Creates Intern Card
-const generateIntern = function(intern) {
+const renderIntern = function(intern) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
@@ -19,7 +19,7 @@ const generateIntern = function(intern) {
 };
 
 // Creates Manager Card
-const generateManager = function(manager) {
+const renderManager = function(manager) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
@@ -38,7 +38,7 @@ const generateManager = function(manager) {
     `
 };
 // Creates Engineer Card
-const generateEngineer = function(engineer) {
+const renderEngineer = function(engineer) {
     return `
     <div class="col-4 mt-4">
         <div class="card h-100">
@@ -58,7 +58,7 @@ const generateEngineer = function(engineer) {
 };
 
 // Function to Render HTML
-function renderHTML(data) {
+renderHTML = (data) => {
     employeeArray = [];
 
     for (let i = 0; i < data.length; i++) {
@@ -83,4 +83,48 @@ function renderHTML(data) {
             employeeArray.push(managerCard);
         }
     }
+
+    const roleCards = employeeArray.join('')
+    const renderTeam = renderTeamHTML(roleCards);
+    return renderTeam;
 }
+
+// HTML Template
+
+const renderTeamHTML = function (roleCards) {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Team</title>
+    <link ref="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link ref="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link ref="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap">
+    <link ref="stylesheet" href="dist/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar" id="navbar">
+            <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">My Team</span>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row justify-content-center" id="team-cards">
+                ${roleCards}
+            </div>
+        </div>
+    </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</html>
+`;
+}
+
+// Export to Index
+module.exports = remderHTML;
