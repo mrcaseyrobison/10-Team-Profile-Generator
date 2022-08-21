994fef7252511f2bb1cffab7b32b7dea45145a0c
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-08-21 15:48:07 -0700

    Added Manager Input

--- index.js
+++ index.js
@@ -13,4 +13,43 @@ const teamArray = [];
 // HTML Generator
 const renderHTML = require("src/renderHTML.js");
 
+// Manager Prompt
+const manager = () => {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "Please add info for the manager of this team.",
+            validate: managerName => {
+                if (managerName) {
+                    return true;
+                } else {
+                    console.log ("You must provide information in this prmpt!");
+                    return false;
+                }
+            }     
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the managers office number?",
+            validate: managerName => {
+                if (isNaN(managerName)) {
+                    console.log ("You must enter a number")
+                     return false;
+                } else {
+                    return true;
+            }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, officeNumber } = managerInput;
+        const manager = new Manager (name, id, email, officeNumber);
 
+        teamArray.push(manager);
+        console.log(manager);
+    })
+};
+
+// Other Employee Prompts
