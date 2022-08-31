44fe49a3a33e20f9421ad22d6b55a17e5769d7e2
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-08-31 11:38:39 -0700

    Engineer test wors

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -7,3 +7,19 @@ test ('creates an Engineer object', () => {
     expect(engineer.github) .toEqual(expect.any(String));
 });
 
+// Tests gitHub username
+
+test ('gets engineer gitHub value', () => {
+    const engineer = new Engineer('Name', 50, 'name@company.com', 'username');
+
+    expect(engineer.getGitHub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+// Tests role
+
+test ('gets role of employee', () => {
+    const engineer = new Engineer('Name', 50, 'name@company.com', 'username');
+
+    expect(engineer.getRole()).toEqual('Engineer');
+});
+
