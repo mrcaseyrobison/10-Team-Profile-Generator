cf20f4e3e02b1e4079a990805379688c71ccb4a4
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-08-31 11:53:06 -0700

    All tests passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,5 +2,14 @@ const Manager = require ('../lib/manager.js');
 
 // Tests Manager Object
 test ('creates a Manager object', () => {
-    
-})
+    const manager = new Manager('Name', 50, 'name@company.com', 2062008000);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// Tests Role
+test ('gets role of Employee', () => {
+    const manager = new Manager('Name', 50, 'name@company.com', 2062008000);
+
+    expect(manager.getRole()).toEqual('Manager');
+});
